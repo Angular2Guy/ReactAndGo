88b99ed3a4f7d785dde307901d39d5b01fc86a34
angular2guy <angular2guy@gmail.com>
2023-01-02 09:35:37 +0100

    feat: add routes

--- backend/pkd/contr/gas-station-controller.go
+++ backend/pkd/contr/gas-station-controller.go
@@ -1,7 +1,35 @@
 package contr
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Start() {
-	fmt.Println("Hello contr")
+	r := gin.Default()
+	r.POST("/posts", postsCreate)
+	r.GET("/posts", postsIndex)
+	r.GET("/posts/:id", postsShow)
+	r.PUT("/posts/:id", postsUpdate)
+	r.DELETE("posts/:id", postsDelete)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func postsCreate(c *gin.Context) {
+
+}
+
+func postsIndex(c *gin.Context) {
+
+}
+
+func postsShow(c *gin.Context) {
+
+}
+
+func postsUpdate(c *gin.Context) {
+
+}
+
+func postsDelete(c *gin.Context) {
+
 }
