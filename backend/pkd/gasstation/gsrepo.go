31eeb8fbe53529a758350d10f10349269df0e9a0
angular2guy <angular2guy@gmail.com>
2023-01-05 18:54:33 +0100

    feat: return more results

--- backend/pkd/gasstation/gsrepo.go
+++ backend/pkd/gasstation/gsrepo.go
@@ -43,7 +43,7 @@ func FindBySearchPlace(searchPlace gsbody.SearchPlaceBody) []gsmodel.GasStation 
 	if len(strings.TrimSpace(searchPlace.StationName)) >= 2 {
 		query = query.Where("name LIKE ?", "%"+strings.TrimSpace(searchPlace.StationName)+"%")
 	}
-	query.Limit(20).Preload("GasPrices", func(db *gorm.DB) *gorm.DB {
+	query.Limit(200).Preload("GasPrices", func(db *gorm.DB) *gorm.DB {
 		return db.Order("date DESC").Limit(20)
 	}).Find(&gasStations)
 	return gasStations
@@ -66,7 +66,7 @@ func FindBySearchLocation(searchLocation gsbody.SearchLocation) []gsmodel.GasSta
 	minMax = updateMinMaxSquare(westLat, westLng, minMax)
 	//fmt.Printf("WestLat: %v, WestLng: %v\n", westLat, westLng)
 	//fmt.Printf("MinLat: %v, MinLng: %v, MaxLat: %v, MaxLng: %v\n", minMax.MinLat, minMax.MinLng, minMax.MaxLat, minMax.MaxLng)
-	database.DB.Where("lat >= ? and lat <= ? and lng >= ? and lng <= ?", minMax.MinLat, minMax.MaxLat, minMax.MinLng, minMax.MaxLng).Limit(100).Preload("GasPrices", func(db *gorm.DB) *gorm.DB {
+	database.DB.Where("lat >= ? and lat <= ? and lng >= ? and lng <= ?", minMax.MinLat, minMax.MaxLat, minMax.MinLng, minMax.MaxLng).Limit(200).Preload("GasPrices", func(db *gorm.DB) *gorm.DB {
 		return db.Order("date DESC").Limit(20)
 	}).Find(&gasStations)
 	return gasStations
