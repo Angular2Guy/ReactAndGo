cb9704ac3c8a255e40c9b03be018a61fc8960d88
angular2guy <angular2guy@gmail.com>
2023-01-06 17:01:41 +0100

    feat: marschal json

--- backend/pkd/contr/client/gsclient.go
+++ backend/pkd/contr/client/gsclient.go
@@ -1,6 +1,7 @@
 package gsclient
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +9,31 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
+type gsResponse struct {
+	Ok       bool         `json:"ok"`
+	License  string       `json:"license"`
+	Data     string       `json:"data"`
+	Status   string       `json:"status"`
+	Stations []gsStations `json:"stations"`
+}
+
+type gsStations struct {
+	Id          string  `json:"id"`
+	Name        string  `json:"name"`
+	Brand       string  `json:"brand"`
+	Street      string  `json:"street"`
+	Place       string  `json:"place"`
+	Lat         float64 `json:"lat"`
+	Lng         float64 `json:"lng"`
+	Dist        float64 `json:"dist"`
+	Diesel      float64 `json:"diesel"`
+	E5          float64 `json:"e5"`
+	E10         float64 `json:"e10"`
+	IsOpen      bool    `json:"isOpen"`
+	HouseNumber string  `json:"houseNumber"`
+	PostCode    int     `json:"postCode"`
+}
 
 func UpdateGsPrices(c *gin.Context) {
 	//func UpdateGsPrices(latitude float64, longitude float64, radiusKM float64) {
@@ -17,12 +43,19 @@ func UpdateGsPrices(c *gin.Context) {
 	var queryUrl = fmt.Sprintf("https://creativecommons.tankerkoenig.de/json/list.php?lat=%f&lng=%f&rad=%f&sort=dist&type=all&apikey=00000000-0000-0000-0000-000000000002", latitude, longitude, radiusKM)
 	response, err := http.Get(queryUrl)
 	if err != nil {
-		log.Fatalf("Request failed: %v", err.Error())
+		log.Fatalf("Request failed: %v\n", err.Error())
 	}
 	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatalf("Read body failed: %v", body)
-	}
 	defer response.Body.Close()
-	fmt.Printf("Body received: %v", string(body))
+	if err != nil {
+		log.Fatalf("Read body failed: %v\n", body)
+	}
+	//fmt.Printf("Body received: %v", string(body))
+	var myGsResponse gsResponse
+	err1 := json.Unmarshal(body, &myGsResponse)
+	if err1 != nil {
+		log.Fatalf("Error: %v\n", err1.Error())
+	}
+	result, _ := json.Marshal(myGsResponse)
+	fmt.Printf("Json: %v\n", string(result))
 }
