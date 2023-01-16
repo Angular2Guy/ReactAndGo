985a40e99213598a64ad10e5d898ee98e1dfcfdd
angular2guy <angular2guy@gmail.com>
2023-01-16 11:03:31 +0100

    feat: users first step

--- backend/pkd/database/migrate.go
+++ backend/pkd/database/migrate.go
@@ -1,7 +1,16 @@
 package database
 
+import "angular-and-go/pkd/gasstation/gsmodel"
+
 //import "angular-and-go/pkd/gasstation/gsmodel"
 
 func MigrateDB() {
 	//DB.AutoMigrate(&gsmodel.GasStation{})
+	if !DB.Migrator().HasTable(&gsmodel.GasStation{}) {
+		DB.AutoMigrate(&gsmodel.GasStation{})
+	}
+	if !DB.Migrator().HasTable(&gsmodel.GasPrice{}) {
+		DB.AutoMigrate(&gsmodel.GasPrice{})
+	}
+
 }
