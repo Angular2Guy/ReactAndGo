82cd69f8e0a53cbd4aa7da4ee9d95fef8bda8fd9
Angular2guy <angular2guy@gmail.com>
2023-02-02 10:02:21 +0100

    fix: entity

--- backend/pkd/gasstation/gsmodel/gasstation.go
+++ backend/pkd/gasstation/gsmodel/gasstation.go
@@ -9,17 +9,17 @@ type GasStation struct {
 	Version                 string
 	VersionTime             time.Time
 	StationName             string `gorm:"column:name"`
-	Brand                   string
+	Brand                   string `gorm:"index:idx_brand"`
 	Street                  string
 	Place                   string
 	HouseNumber             string
-	PostCode                string
-	Latitude                float64 `gorm:"column:lat"`
-	Longitude               float64 `gorm:"column:lng"`
+	PostCode                string  `gorm:"index:idx_gas_station_post_code"`
+	Latitude                float64 `gorm:"column:lat;index:idx_lat"`
+	Longitude               float64 `gorm:"column:lng;index:idx_lng"`
 	PublicHolidayIdentifier string
-	PriceInImport           time.Time
+	PriceInImport           time.Time `gorm:"index:idx_updated"`
 	PriceChanged            time.Time
-	OpenTs                  int
+	OpenTs                  int `gorm:"index:idx_open_ts"`
 	OtJson                  string
 	StationInImport         time.Time
 	FirstActive             time.Time
