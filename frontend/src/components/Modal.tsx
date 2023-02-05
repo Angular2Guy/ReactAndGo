92c182c20d7d8c4e918547b821154c56211c4dfe
Angular2guy <angular2guy@gmail.com>
2023-02-05 16:53:44 +0100

    feat: login cancel

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -15,12 +15,18 @@ const Modal = () => {
       setGlobalUserName(userName);
       setUserName('');
   }
-
+  const handleCancel = (event: React.FormEvent) => {
+   event.preventDefault();
+      setUserName('');
+  }
      return createPortal(
       <div className={styles.modalForm}>
       <form onSubmit={handleSubmit}>
           <input value={userName} onChange={handleChange} className={styles.userNameInput} placeholder="Username"></input>
+          <div className={styles.modalButtons}>
           <button type="submit" className={styles.loginButton}>Login</button>
+          <button className={styles.cancelButton} onClick={handleCancel}>Cancel</button>
+          </div>
          <div>GlobalUserName: {globalUserName}</div>
       </form>
   </div>, document.getElementById('modal') as Element
