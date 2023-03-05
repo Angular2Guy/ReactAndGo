fa7b3cc53ca46a4b2af54326faeb2a4255d4ab50
Angular2guy <angular2guy@gmail.com>
2023-03-05 11:44:44 +0100

    feat: build next step

--- backend/pkd/controller/basecontroller.go
+++ backend/pkd/controller/basecontroller.go
@@ -25,7 +25,7 @@ func Start() {
 	router.POST("/gasstation/search/location", token.CheckToken, searchGasStationLocation)
 	router.GET("/usernotification/new/:useruuid", token.CheckToken, getNewUserNotifications)
 	router.GET("/usernotification/current/:useruuid", token.CheckToken, getCurrentUserNotifications)
-	router.Static("/static", "./static")
-	router.NoRoute(func(c *gin.Context) { c.Redirect(http.StatusTemporaryRedirect, "/static") })
+	router.Static("/public", "./public")
+	router.NoRoute(func(c *gin.Context) { c.Redirect(http.StatusTemporaryRedirect, "/public") })
 	router.Run() // listen and serve on 0.0.0.0:3000
 }
