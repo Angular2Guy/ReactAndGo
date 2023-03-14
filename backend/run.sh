1fb54dc0cb57145da759589b8042a6d686c490b3
Angular2guy <angular2guy@gmail.com>
2023-03-14 19:21:43 +0100

    fix: support different libc versions

--- backend/run.sh
+++ backend/run.sh
@@ -2,7 +2,9 @@
 export GOGC=off
 export GOMEMLIMIT=64MiB
 export GODEBUG=gctrace=1
-#echo $GOGC $GOMEMLIMIT $GODEBUG
+#to support differen libc versions
+export CGO_ENABLED=0
+#echo $GOGC $GOMEMLIMIT $GODEBUG $CGO_ENABLED
 #go build -toolexec=/bin/time
 #./react-and-go
 go run main.go
