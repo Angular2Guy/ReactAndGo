b5882ae7958c38ee36a661db6a3eff3a0a4d970e
Angular2guy <angular2guy@gmail.com>
2024-03-31 14:29:41 +0200

    feat: add gin prod config

--- backend/run.sh
+++ backend/run.sh
@@ -3,6 +3,7 @@ export GOGC=off
 export GOMEMLIMIT=384MiB
 export GODEBUG=gctrace=1
 export GOMAXPROCS=12
+#export GIN_MODE=release
 #to support differen libc versions
 export CGO_ENABLED=0
 #echo $GOGC $GOMEMLIMIT $GODEBUG $CGO_ENABLED
