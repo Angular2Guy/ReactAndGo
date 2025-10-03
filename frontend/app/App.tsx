c419e0ebc61667bc83d22373babcf38c26048476
Angular2guy <angular2guy@gmail.com>
2025-10-03 08:56:05 +0200

    fix: update

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -39,8 +39,7 @@ function App() {
 
   return (
     <div className="App">      
-      <Header/>
-      <LoginModal/>
+      <Header/>      
       <LocationModal/>
       <TargetPriceModal/>
       <Main/>      
