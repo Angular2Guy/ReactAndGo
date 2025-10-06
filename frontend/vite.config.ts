f6f2b6952f954803edff38bddfaebf1c2857cdad
Angular2guy <angular2guy@gmail.com>
2025-10-06 19:50:40 +0200

    fix: libraries

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(), 
     reactRouter(), tsconfigPaths()],   
+  ssr: {
+    // Workaround for resolving dependencies in the server bundle
+    // Without this, the React context will be different between direct import and transitive imports in development environment
+    // For more information, see https://github.com/mui/material-ui/issues/45878#issuecomment-2987441663
+    optimizeDeps: {
+      include: ['@emotion/*', '@mui/*'],
+    },
+    noExternal: ['@emotion/*', '@mui/*'],
+  },
 });
