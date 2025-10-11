7f91847b36684874193f7310b1680924af29a28e
Angular2guy <angular2guy@gmail.com>
2025-10-11 10:32:35 +0200

    feat: add route

---
+++ frontend/app/login/login.tsx
@@ -0,0 +1,8 @@
+export function Login() {
+  return (
+    <div>
+      <h1>Welcome to the App!</h1>
+      <p>This is a simple React Router application.</p>
+    </div>
+  );
+}
--- frontend/app/routes/app.tsx
+++ frontend/app/routes/app.tsx
@@ -1,5 +1,5 @@
-import type { Route } from "./+types/home";
-import { Login } from "~/login/login";
+import type { Route } from "./+types/app";
+import { Welcome } from "~/welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Login />;
+  return <Welcome />;
 }
