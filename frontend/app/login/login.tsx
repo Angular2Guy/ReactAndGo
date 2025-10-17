1b44ab0b4fd9be7ed2387bb2aa13c1c9fe8776ef
Angular2guy <angular2guy@gmail.com>
2025-10-17 21:18:29 +0200

    fix: login

--- frontend/app/login/login.tsx
+++ frontend/app/login/login.tsx
@@ -11,12 +11,15 @@
     limitations under the License.
 */
 import { useNavigate } from "react-router";
-import Button from '@mui/material/Button';
+import { Dialog, DialogContent, Button, Tabs, Tab, Box, TextField } from '@mui/material';
 import * as React from 'react';
 import {useState} from 'react';
-import GlobalState from "~/GlobalState";
+import GlobalState, { type UserDataState } from "~/GlobalState";
 import styles from './login.module.css';
 import { useAtom } from "jotai";
+import type { FormEvent, BaseSyntheticEvent, ChangeEventHandler } from "react";
+import { postLogin, postSignin } from "~/service/http-client";
+import type { UserResponse } from "~/model/user";
 
 interface MsgData {
   jwtToken?: string;
@@ -65,14 +68,188 @@ export function Login() {
   const navToApp = () => {
     navigate("/app/app");
   }
+  
+  const handleChangeUsername: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setUserName(event.currentTarget.value as string);
+  };
+  const handleChangePassword1: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setPassword1(event.currentTarget.value as string);
+  };
+  const handleChangePassword2: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setPassword2(event.currentTarget.value as string);
+  };
 
-  return (
-    <div>
-      <h1>Welcome to the App!</h1>
-      <p>This is a simple React Router application.</p>      
-      <Button variant="contained" color="primary" onClick={navToApp}>
-        Login
-      </Button>
-    </div>
-  );
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    Notification.requestPermission();
+    if(!!controller) {
+      controller.abort();
+    }
+    setResponseMsg('');
+    controller = new AbortController();
+    const userResponse = activeTab === 0 ? await postLogin(userName, password1, controller) : await postSignin(userName, password1, controller);    
+    controller = null;
+    //console.log(userResponse);
+    if (!userResponse?.Message && !!userResponse?.Token && userResponse.Token?.length > 10 && !!userResponse?.Uuid && userResponse.Uuid?.length > 10) {
+      setGlobalUserName(userName);
+      setGlobalJwtToken(userResponse.Token);      
+      setGlobalUuid(userResponse.Uuid);
+      setGlobalUserDataState({
+        Latitude: userResponse.Latitude, Longitude: userResponse.Longitude, SearchRadius: userResponse.SearchRadius, PostCode: userResponse.PostCode,
+        TargetDiesel: userResponse.TargetDiesel, TargetE10: userResponse.TargetE10, TargetE5: userResponse.TargetE5
+      } as UserDataState);
+      setGlobalLoginModal(false);
+      initWebWorker(userResponse);
+      setUserName('');
+      setPassword1('');
+      setPassword2('');
+      navigate('/app');
+    } else if (!!userResponse?.Message) {
+      setResponseMsg(userResponse.Message);
+    }
+  }
+
+  const initWebWorker = async (userResponse: UserResponse) => {
+    let result = null;
+    if (!globalWebWorkerRefState) {
+      const worker = new Worker(new URL('../webpush/dedicated-worker.js', import.meta.url));
+      if (!!worker) {
+        worker.addEventListener('message', (event: MessageEvent) => {
+          //console.log(event.data);
+          if (!!event?.data?.Token && event?.data.Token?.length > 10) {
+            setGlobalJwtToken(event.data.Token);
+            GlobalState.jwtToken = event.data.Token;
+          }
+        });
+        worker.postMessage({ jwtToken: userResponse.Token, newNotificationUrl: `/usernotification/new/${userResponse.Uuid}` } as MsgData);
+        setGlobalWebWorkerRefState(worker);
+        result = worker;
+      }
+    } else {
+      globalWebWorkerRefState.postMessage({ jwtToken: userResponse.Token, newNotificationUrl: `/usernotification/new/${userResponse.Uuid}` } as MsgData);
+      result = globalWebWorkerRefState;
+    }
+    return result;
+  };
+
+  const handleCancel = (event: FormEvent) => {
+    event.preventDefault();
+    setUserName('');
+    setPassword1('');
+    setPassword2('');
+    setResponseMsg('');
+  };
+  const handleClose = () => {
+    //setOpen(false);
+  };
+  const handleTabChange = (event: BaseSyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
+    setResponseMsg('');
+  };
+  const a11yProps = (index: number) => {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+   return (<Dialog open={globalLoginModal} onClose={handleClose} className="backDrop">
+    <DialogContent>
+      <Tabs value={activeTab} onChange={handleTabChange} aria-label="basic tabs example">
+        <Tab label="Login" {...a11yProps(0)} />
+        <Tab label="Singin" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={activeTab} index={0}>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            autoFocus
+            margin="dense"
+            value={userName}
+            onChange={handleChangeUsername}
+            id="userName"
+            label="user name"
+            type="string"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            value={password1}
+            onChange={handleChangePassword1}
+            id="password1"
+            label="password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+          <div>
+            <Button type="submit">Ok</Button>
+            <Button onClick={handleCancel}>Cancel</Button>
+          </div>
+          <div className={styles.responseMsg}>
+            {[responseMsg].filter(value => !!value).map((value, index) =>
+              <span key={index}>Message: {value}</span>
+            )}
+          </div>
+        </Box>
+      </TabPanel>
+      <TabPanel value={activeTab} index={1}>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            autoFocus
+            margin="dense"
+            value={userName}
+            onChange={handleChangeUsername}
+            id="userName"
+            label="user name"
+            type="string"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            value={password1}
+            onChange={handleChangePassword1}
+            id="password1"
+            label="password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            value={password2}
+            onChange={handleChangePassword2}
+            id="password2"
+            label="password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+          <div>
+            <Button type="submit">Ok</Button>
+            <Button onClick={handleCancel}>Cancel</Button>
+          </div>
+          <div className={styles.responseMsg}>
+            {[responseMsg].filter(value => !!value).map((value, index) =>
+              <span key={index}>Message: {value}</span>
+            )}
+          </div>
+        </Box>
+      </TabPanel>
+    </DialogContent>
+  </Dialog>);
 }
