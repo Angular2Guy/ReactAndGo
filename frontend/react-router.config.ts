9df9c0762572dcaed9a75a5cd3af9138c4c2f5e2
Angular2guy <angular2guy@gmail.com>
2025-10-21 20:50:06 +0200

    fix: make it an spa

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -15,5 +15,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
 } satisfies Config;
